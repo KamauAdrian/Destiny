180410ca7b3b792009801802df1d97dad7a7f3a1
Adrian Kamau <adriankubai1999@gmail.com>
2018-03-29 13:41:34 +0300

    Destiny

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+</head>
+<body>
+	<div id="navone" class="container-fluid navbar-fixed-top">
+	<div class="row">
+		<div class="col-md-12">
+	<nav class=" navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">MY FIRST WEBSITE</a>
+			</div>
+			<div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#home"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+					<li><a href="#">Gallery</a></li>
+					<li><a href="#">Contact us</a></li>
+					<li><a href="#">About us</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class=" glyphicon glyphicon-user"></span> Sign up</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</div>
+</div>
+</div>
+
+
+<div class="container-fluid" id="home">
+		<div class="row">
+		<div class="col-md-12">
+	<center>
+	<h1 class="t1">WELCOME TO DESTINY STORES</h1>
+	</center>
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/12.jpg" alt="IMG-001" title="IMG-001" id="wows1_0"/></li>
+		<li><img src="data1/images/13.jpg" alt="IMG-002" title="IMG-002" id="wows1_1"/></li>
+		<li><img src="data1/images/112.jpg" alt="IMG-003" title="IMG-003" id="wows1_2"/></li>
+		<li><img src="data1/images/113.jpg" alt="IMG-004" title="IMG-004" id="wows1_3"/></li>
+		<li><img src="data1/images/114.jpg" alt="IMG-005" title="IMG-005" id="wows1_4"/></li>
+		<li><a href="http://wowslider.net"><img src="data1/images/120.jpg" alt="javascript carousel" title="IMG-006" id="wows1_5"/></a></li>
+		<li><img src="data1/images/121.jpg" alt="IMG-007" title="IMG-007" id="wows1_6"/></li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title="IMG-001"><span><img src="data1/tooltips/12.jpg" alt="IMG-001"/>1</span></a>
+		<a href="#" title="IMG-002"><span><img src="data1/tooltips/13.jpg" alt="IMG-002"/>2</span></a>
+		<a href="#" title="IMG-003"><span><img src="data1/tooltips/112.jpg" alt="IMG-003"/>3</span></a>
+		<a href="#" title="IMG-004"><span><img src="data1/tooltips/113.jpg" alt="IMG-004"/>4</span></a>
+		<a href="#" title="IMG-005"><span><img src="data1/tooltips/114.jpg" alt="IMG-005"/>5</span></a>
+		<a href="#" title="IMG-006"><span><img src="data1/tooltips/120.jpg" alt="IMG-006"/>6</span></a>
+		<a href="#" title="IMG-007"><span><img src="data1/tooltips/121.jpg" alt="IMG-007"/>7</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">html5 slider</a> by WOWSlider.com v8.8</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+
+
+		</div> 
+	</div>
+</div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+</body>
+</html>
